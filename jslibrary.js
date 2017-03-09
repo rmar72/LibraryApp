68efb8b3a289e559b94ad11dc9f4df77f0000f03
DESKTOP-D3L4DSU\Student8 <rubensol686@gmail.com>
2017-03-09 13:11:24 -0700

    Changed naming convention

--- jslibrary.js
+++ jslibrary.js
@@ -29,48 +29,48 @@ var jsLib = new Lib("JS303", "Denver", "CO");
 Lib.prototype.bookArr = [];
 
 Lib.prototype.addBook = function(newBook){
-	var res = true;
+	var isDecide = true;
 	if(typeof newBook !== "object"){
-		return "Not a valid entry.";
+		throw "Not a valid entry.";
 	}	
 		
 	for(var i=0; i < this.bookArr.length; i++){
 		if(this.bookArr[i].title == newBook.title){
-			res = false;
+			isDecide = false;
 		}
 	}	
-	return res ? (this.bookArr.push(newBook), true) : false;
+	return isDecide ? (this.bookArr.push(newBook), true) : false;
 }
 
 Lib.prototype.removeBookByTitle = function(str){
-	var res = false;
+	var isDecide = false;
 	if(typeof str !== "string"){
-		return "Not a valid entry.";
+		throw "Not a valid entry.";
 	}
 	
 	for(var i=0; i < this.bookArr.length; i++){
 		if(this.bookArr[i].title == str){
 			this.bookArr.splice(i,1);
-			res = true; 
+			isDecide = true; 
 		}	
 	}
-	return res ? true : false;
+	return isDecide ? true : false;
 }
 
 Lib.prototype.removeBookByAuthor = function(authorName){
-	var res = false;
+	var isDecide = false;
 	if( typeof authorName !== "string" ){
-		return "Not a valid entry.";
+		throw "Not a valid entry.";
 	}	
 	
 	for(var i=0; i < this.bookArr.length; i++){
 		if(this.bookArr[i].author == authorName){
 			this.bookArr.splice(i,1);
-			res = true;
+			isDecide = true;
 			i--; //makes up for the missing index that was removed
 		}	
 	}
-	return res ? true : false;
+	return isDecide ? true : false;
 }
 
 Lib.prototype.getRandomBook = function(){
@@ -78,33 +78,33 @@ Lib.prototype.getRandomBook = function(){
 }
 
 Lib.prototype.getBookByTitle = function(title){
-	var reg = new RegExp(title, 'gi'), arr1 = [];
+	var regex1 = new RegExp(title, 'gi'), aTitles = [];
 	
 	if(typeof title !== "string"){
-		return "Not a valid entry.";
+		throw "Not a valid entry.";
 	}
 
 	for(var i=0; i < this.bookArr.length; i++){
-		if(this.bookArr[i].title.match(reg)){
-			arr1.push( this.bookArr[i] );
+		if(this.bookArr[i].title.match(regex1)){
+			aTitles.push( this.bookArr[i] );
 		}
 	}	
-	return arr1;
+	return aTitles;
 }
 
 Lib.prototype.getBooksByAuthor = function(authorName){
-	var reg = new RegExp(authorName, 'gi'), arr1 = [];
+	var regex1 = new RegExp(authorName, 'gi'), aAuthors = [];
 		
 	if(typeof authorName !== "string"){
-		return "Not a valid entry.";
+		throw "Not a valid entry.";
 	}
 	
 	for(var i=0; i < this.bookArr.length; i++){
-		if( this.bookArr[i].author.match(reg) ){
-			arr1.push( this.bookArr[i] );	
+		if( this.bookArr[i].author.match(regex1) ){
+			aAuthors.push( this.bookArr[i] );	
 		}
 	}
-	return arr1;
+	return aAuthors;
 }
 
 Lib.prototype.addBooks = function(books){
@@ -112,19 +112,19 @@ Lib.prototype.addBooks = function(books){
 	for(var i=0; i < books.length; i++){
 		this.addBook(books[i]);
 	}
-	return this.bookArr.length-oldLength;
+	return this.bookArr.length - oldLength;
 }
 
 Lib.prototype.getAuthors = function(){
-	var arr1 = [];
+	var aAuthors = [];
 	
 	for(var i=0; i < this.bookArr.length; i++){
 		var oBookAuthor = this.bookArr[i].author;
-		if(arr1.indexOf(oBookAuthor) <= -1){
-			arr1.push(oBookAuthor);
+		if(aAuthors.indexOf(oBookAuthor) <= -1){
+			aAuthors.push(oBookAuthor);
 		}
 	}	
-	return arr1;
+	return aAuthors;
 }
 
 Lib.prototype.getRandomAuthors = function(){
